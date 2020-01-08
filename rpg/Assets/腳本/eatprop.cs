5bab73fc33cde6a6b11c458d031b21f1d84eace6
tony106051975 <55470420+tony106051975@users.noreply.github.com>
2020-01-08 12:04:23 +0800

    0.0

---
+++ rpg/Assets/腳本/eatprop.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class eatprop : MonoBehaviour
+{
+    [Header("道具數量")]
+    public Text textCount;
+    [Header("提示畫面")]
+    public GameObject objTip;
+
+    public int propCount;
+    public int propTotal = 10;
+
+    public bool finish;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "洋蔥")
+        {
+            Destroy(collision.gameObject);
+            propCount++;
+            textCount.text = "道具數 :" + propCount;
+
+            if (propCount == propTotal)
+            {
+                finish = true;
+                objTip.SetActive(true);
+            }
+
+        }
+    }
+        private void Update()
+        {
+            GameOver();
+        }
+        private void GameOver()
+        {
+            if (finish)
+            {
+                if (Input.GetKeyDown(KeyCode.Escape))
+                {
+                    Application.Quit();
+                }
+                if (Input.GetKeyDown(KeyCode.R))
+                {
+                    SceneManager.LoadScene("SampleScene");
+                }
+            }
+        }
+
+
+
+
+
+    }
---
+++ rpg/Assets/腳本/player.cs
@@ -0,0 +1,26 @@
+﻿
+using UnityEngine;
+
+public class player : MonoBehaviour
+{
+    [Header("動畫參數名稱")]
+
+    public string perRun = "跑步開關";
+
+
+
+    public Animator ani;
+
+    private void Run()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        ani.SetBool("跑步開關", h != 0 || v != 0);
+    }
+    private void Update()
+    {
+        Run();
+
+    }
+}
